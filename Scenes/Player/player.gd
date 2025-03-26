d61ad3820be9d0f26ea4fac789ace4706f3e5957
MarkFitzgeraldCode_Blooded <markfitz2014@gmail.com>
2025-03-26 12:56:35 +0000

    Added gravity mod var.
    Added gravity mod and set it in gravity.

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -4,12 +4,14 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.9
 
+var gravity_mod = 2.0
+
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * gravity_mod
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
